e81cf3ff0870765e486891a277610ea851621703
Didier K. Nzimbi <82230373+dbahsa@users.noreply.github.com>
2021-09-09 15:29:23 +0200

    next button ok

--- js/app.js
+++ js/app.js
@@ -1,5 +1,18 @@
 const images = document.querySelectorAll('#gallery img');
 
+let imgActive = 0;
+
+// Hide the 3 last pictures
 for (let i = 4; i < images.length; i++) {
     images[i].classList.add('hidden');
 }
+
+//Click on 'next' button 0123456
+document.querySelector('#next').addEventListener('click', function() {
+    for (let i = 0; i < 3; i++) {
+        images[i].classList.add('hidden');
+    }
+    for (let i = 4; i < images.length; i++) {
+        images[i].classList.remove('hidden');
+    }
+});
